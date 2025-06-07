30a3e34ce2531f2491f67e71cc4494ba8d0d175b
xiaoguan <xiaoguanstudio@outlook.com>
2025-06-07 23:05:58 +0800

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -6,9 +6,9 @@ export default async (ctx) => {
     const p = new Providers()
 
     const query = ctx.req.query()
-    const server = query.server || 'tencent'
+    const server = query.server || 'netease'
     const type = query.type || 'playlist'
-    const id = query.id || '7326220405'
+    const id = query.id || '9564899591'
 
     if (!p.get_provider_list().includes(server) || !p.get(server).support_type.includes(type)) {
         ctx.status(400)
