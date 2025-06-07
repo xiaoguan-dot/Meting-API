6ad232a266365c05af342572e3260938010fedb5
xiaoguan <xiaoguanstudio@outlook.com>
2025-06-08 02:26:22 +0800

    Update template.js

--- src/template.js
+++ src/template.js
@@ -1,4 +1,80 @@
 import example from "./example.js"
+export const handler = (c) => {
+    return c.html(`<!DOCTYPE html>
+<html lang="zh-CN" id="htmlid">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>小关のMeting | Test/title>
+    <link rel="stylesheet" href="https://unpkg.com/zui@3.0.0/dist/zui.css">
+    <link rel="stylesheet" href="https://unpkg.com/aplayer/dist/APlayer.min.css">
+</head>
+
+<body>
+    <style>
+        .audiolist {
+            padding: 20px;
+            margin: 30px;
+        }
+        .style-group { position: fixed; right: 30px; top: 30px }
+        @media (max-width: 629px) { .style-group { position: fixed; right: auto; top: auto; bottom: 20px; left: calc(50% - 50px); justify-content: center; } }
+        #htmlid.dark {
+            .aplayer { background: #1e293b; }
+            .aplayer .aplayer-lrc:before { background: linear-gradient(to bottom, rgb(30, 41, 59) 0%, rgba(255, 255, 255, 0) 100%); }
+            .aplayer .aplayer-lrc:after { background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(30, 41, 59, 0.726) 100%); } }
+    </style>
+    <div class="btn-group style-group">
+        <button class="btn" type="button" onclick="style_light()" id="style_light"><i
+                class="icon icon-sun"></i></button>
+        <button class="btn" type="button" onclick="style_dark()" id="style_dark"><i class="icon icon-moon"></i></button>
+        <button class="btn active" type="button" onclick="style_auto()" id="style_auto"><i
+                class="icon icon-desktop"></i></button>
+    </div>
+    <script src="https://unpkg.com/zui@3.0.0/dist/zui.js"></script>
+    <script src="https://unpkg.com/aplayer/dist/APlayer.min.js"></script>
+    <script>
+        var meting_api = 'api?server=:server&type=:type&id=:id&auth=:auth&r=:r';
+    </script>
+    <script src="https://unpkg.com/@xizeyoupan/meting@latest/dist/Meting.min.js"></script>
+    <h1 style="padding: 30px;padding-bottom: 15px;">Meting API | 测试</h1>
+    <ol class="breadcrumb" style="padding: 30px;padding-top: 0px;padding-bottom: 0px;">
+    <li><a href="/">首页</a></li>
+    <li class="active">测试</li>
+    </ol>
+    <div class="audiolist">
+        `
+        ,Object.keys(example).map(provider => {
+        Object.keys(example[provider]).map(type => {
+        if (!example[provider][type].show) return
+
+        html += `
+        <div>
+            <p>${provider} ${type}</p>
+            <meting-js server="${provider}" type="${type}" id="${example[provider][type].value}" list-folded=true />
+        </div>
+        <br />
+        `
+        })
+        })
+        ,html += `
+    </div>
+</body>
+<script>
+    // 主题设置
+    // 暗色主题判断
+    let isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches; let element = document.getElementById('htmlid'); element.classList.toggle('dark', isDarkMode); element.classList.toggle('light', !isDarkMode);
+    // 自动  
+    function style_auto() { let click = document.getElementById('style_auto'); click.classList.add("active"); let rd = document.getElementById('style_dark'); rd.classList.remove("active"); let rl = document.getElementById('style_light'); rl.classList.remove("active"); let isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches; let element = document.getElementById('htmlid'); element.classList.toggle('dark', isDarkMode); element.classList.toggle('light', !isDarkMode); }
+    // 暗色  
+    function style_dark() { let click = document.getElementById('style_dark'); click.classList.add("active"); let rl = document.getElementById('style_light'); rl.classList.remove("active"); let ra = document.getElementById('style_auto'); ra.classList.remove("active"); let element = document.getElementById('htmlid'); element.className = "dark"; }
+    // 亮色  
+    function style_light() { let click = document.getElementById('style_light'); click.classList.add("active"); let rd = document.getElementById('style_dark'); rd.classList.remove("active"); let ra = document.getElementById('style_auto'); ra.classList.remove("active"); let element = document.getElementById('htmlid'); element.className = "light"; }
+</script>
+</script>
+
+</html>`)
+}
 
 export const docs = (c) => {
     return c.html(`<!doctype html>
@@ -137,70 +213,3 @@ export const docs = (c) => {
 
 </html>`)
 }
-export const handler = (c) => {
-    return c.html(`<!DOCTYPE html>
-<html lang="zh-CN" id="htmlid">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>小关のMeting | Test/title>
-    <link rel="stylesheet" href="https://unpkg.com/zui@3.0.0/dist/zui.css">
-    <link rel="stylesheet" href="https://unpkg.com/aplayer/dist/APlayer.min.css">
-</head>
-
-<body>
-    <style>
-        .audiolist {
-            padding: 20px;
-            margin: 30px;
-        }
-        .style-group { position: fixed; right: 30px; top: 30px }
-        @media (max-width: 629px) { .style-group { position: fixed; right: auto; top: auto; bottom: 20px; left: calc(50% - 50px); justify-content: center; } }
-        #htmlid.dark {
-            .aplayer { background: #1e293b; }
-            .aplayer .aplayer-lrc:before { background: linear-gradient(to bottom, rgb(30, 41, 59) 0%, rgba(255, 255, 255, 0) 100%); }
-            .aplayer .aplayer-lrc:after { background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(30, 41, 59, 0.726) 100%); } }
-    </style>
-    <div class="btn-group style-group">
-        <button class="btn" type="button" onclick="style_light()" id="style_light"><i
-                class="icon icon-sun"></i></button>
-        <button class="btn" type="button" onclick="style_dark()" id="style_dark"><i class="icon icon-moon"></i></button>
-        <button class="btn active" type="button" onclick="style_auto()" id="style_auto"><i
-                class="icon icon-desktop"></i></button>
-    </div>
-    <script src="https://unpkg.com/zui@3.0.0/dist/zui.js"></script>
-    <script src="https://unpkg.com/aplayer/dist/APlayer.min.js"></script>
-    <script>
-        var meting_api = 'api?server=:server&type=:type&id=:id&auth=:auth&r=:r';
-    </script>
-    <script src="https://unpkg.com/@xizeyoupan/meting@latest/dist/Meting.min.js"></script>
-    <h1 style="padding: 30px;padding-bottom: 15px;">Meting API | 测试</h1>
-    <ol class="breadcrumb" style="padding: 30px;padding-top: 0px;padding-bottom: 0px;">
-    <li><a href="/">首页</a></li>
-    <li class="active">测试</li>
-    </ol>
-    <div class="audiolist"> 
-        `Object.keys(example).map(provider => {
-        Object.keys(example[provider]).map(type => {
-        if (!example[provider][type].show) return
-        html += `
-        <div><p>${provider} ${type}</p><meting-js server="${provider}" type="${type}" id="${example[provider][type].value}" list-folded=true /></div><br/>`})})html += `
-    </div>
-</body>
-<script>
-    // 主题设置
-    // 暗色主题判断
-    let isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches; let element = document.getElementById('htmlid'); element.classList.toggle('dark', isDarkMode); element.classList.toggle('light', !isDarkMode);
-    // 自动  
-    function style_auto() { let click = document.getElementById('style_auto'); click.classList.add("active"); let rd = document.getElementById('style_dark'); rd.classList.remove("active"); let rl = document.getElementById('style_light'); rl.classList.remove("active"); let isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches; let element = document.getElementById('htmlid'); element.classList.toggle('dark', isDarkMode); element.classList.toggle('light', !isDarkMode); }
-    // 暗色  
-    function style_dark() { let click = document.getElementById('style_dark'); click.classList.add("active"); let rl = document.getElementById('style_light'); rl.classList.remove("active"); let ra = document.getElementById('style_auto'); ra.classList.remove("active"); let element = document.getElementById('htmlid'); element.className = "dark"; }
-    // 亮色  
-    function style_light() { let click = document.getElementById('style_light'); click.classList.add("active"); let rd = document.getElementById('style_dark'); rd.classList.remove("active"); let ra = document.getElementById('style_auto'); ra.classList.remove("active"); let element = document.getElementById('htmlid'); element.className = "light"; }
-</script>
-</script>
-
-</html>`)
-}
-
